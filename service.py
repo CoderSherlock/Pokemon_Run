ca755981f16e278b0ea52b624d740476a6d3d4fa
CoderSherlock <haopengzhan@gmail.com>
2016-07-30 15:31:16 -0400

    add getting player info

--- service.py
+++ service.py
@@ -25,13 +25,24 @@ from POGOProtos.Networking.Requests.Messages import NicknamePokemonMessage_pb2
 
 API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
 
+# TODO -> Hide errors: From ### 
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 
 class Service:
     def __init__(self, token=None, location_name=""):
         self.access_token = token
         self.origin_location = location_name
         self.authTicket = None
-        self.session = None
+
+        # Create Niantic Session
+        self.session = requests.session()
+        self.session.headers = {
+                'User-Agent': 'Niantic App'
+                }
+        self.session.verify = False
+
         self.start_process(token, location_name)
 
     def restart_process(self):
@@ -43,11 +54,14 @@ class Service:
         self.location.set_location_name(location_name);     # Get Location at here
         print('[SE]\tGoogle token: '+ token[:40])
 
-        state = status.Status() 
+        self.state = status.Status() 
         self.endpoint = 'https://{0}{1}'.format(self.createApiEndpoint(),'/rpc')
 
+        print("Start grab player infos")
         payload = [Request_pb2.Request(request_type = RequestType_pb2.GET_PLAYER)]
-        res = self.wrap_and_request() 
+        res = self.wrap_and_request(payload) 
+        self.state.profile.ParseFromString(res.returns[0])
+        print(self.state.profile)
 
     def createApiEndpoint(self):
         payload = []
@@ -59,8 +73,9 @@ class Service:
             print("Servers are busy now. Exiting")
             sys.exit("Can't connect to server in creating api endpoint, Error 51263")
         return res.api_url
-    
+
     def wrap_in_request(self, payload):
+        info = None
         if not self.authTicket:
             info = RequestEnvelope_pb2.RequestEnvelope.AuthInfo(
                     provider = "google",
@@ -69,7 +84,7 @@ class Service:
                         unknown2 = 59
                         )
                     )
-        # Build Envelpe
+            # Build Envelpe
         # TODO -> This parts need new method to fix multiple authentication
         req = RequestEnvelope_pb2.RequestEnvelope(
                 status_code = 2,
@@ -81,7 +96,7 @@ class Service:
                 unknown12 = 989,
                 auth_info = info
                 )
-    
+
         data = [None, ] * 4
         data[0] = Request_pb2.Request(
                 request_type = RequestType_pb2.GET_HATCHED_EGGS
@@ -106,21 +121,41 @@ class Service:
         return req
 
     def request(self, req, url=None):
-        #try:
+        try:
             if url is None:
                 url = self.endpoint
             # Send request
             rawResponse = self.session.post(url, data=req.SerializeToString())
 
-            res = RequestEnvelope_pb2.ResponseEnvelope()
+            res = ResponseEnvelope_pb2.ResponseEnvelope()
             res.ParseFromString(rawResponse.content)
 
             if res.auth_ticket.start:
                 self.authTicket = res.auth_ticket
             return res
-        #except:
+        except:
             sys.exit("Can't acquire request from server, Error 64114")
             return 1
 
-    def wrap_and_request(self):
-        return 1
+    def wrap_and_request(self, payload):
+        res = self.request(self.wrap_in_request(payload))
+
+        if res is not None:
+            self.parse_default(res)
+        else:
+            sys.exit("Error to get response froom server, Error 12552")
+        return res
+
+    def parse_default(self, res):
+        #try:
+        self.state.eggs.ParseFromString(res.returns[1])
+        self.state.inventory.ParseFromString(res.returns[2])
+        self.state.badges.ParseFromString(res.returns[3])
+        self.state.settings.ParseFromString(res.returns[4])
+        #except:
+            #sys.exit("Error parsing response. Malformed response, Error 84923")
+        
+        # Finally make inventory usable
+        item = self.state.inventory.inventory_delta.inventory_items
+        #self.inventory = Inventory(item)
+
